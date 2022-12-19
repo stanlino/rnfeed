f6c3fb312c9f597928e9a73a325b09cb84ffa388
Stanley Filipe <stanley92filipe@gmail.com>
2022-12-19 17:44:17 -0300

    feat: logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
 import {
-  Container, Title
+  Container, 
+  Icon,
 } from './styles'
 
 export function Logo(){
   return (
     <Container>
-      <Title>
-        RNFeed
-      </Title>
+      <Icon name="timeline" />
     </Container>
   )
 }
--- src/components/logo/styles.ts
+++ src/components/logo/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export const Container = styled.View`
   align-items: center;
   justify-content: center;
 `
 
-export const Title = styled.Text`
+export const Icon = styled(MaterialCommunityIcons)`
   font-size: 24px;
   font-weight: bold;
   color: ${({ theme }) => theme.colors.text};
